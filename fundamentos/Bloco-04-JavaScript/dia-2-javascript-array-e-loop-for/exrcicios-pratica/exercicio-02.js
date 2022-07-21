ade4bed14f70c8a31f840a7f7aaea448a71901ed
NataliaArmond <taya.armond@gmail.com>
2022-07-21 19:24:40 -0300

    exercicio2 soma dos valores da array

--- fundamentos/Bloco-04-JavaScript/dia-2-javascript-array-e-loop-for/exrcicios-pratica/exercicio-02.js
+++ fundamentos/Bloco-04-JavaScript/dia-2-javascript-array-e-loop-for/exrcicios-pratica/exercicio-02.js
@@ -1,6 +1,8 @@
 // Iremos utilizar esse array 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// exercicio: some todos os valores contidos no array e imprima o resultado;
+let soma = 0
 for (let index = 0; index < numbers.length; index+=1) {
-    console.log(numbers[index]);
+    soma += numbers[index];
 }
+console.log(soma);
