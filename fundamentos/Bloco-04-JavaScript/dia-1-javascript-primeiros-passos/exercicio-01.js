6775b9708b4ae97b1f7f05ad95d96b91e6543f0c
NataliaArmond <taya.armond@gmail.com>
2022-07-21 10:32:28 -0300

    cria arquivo html cria arquivo javaScript realizando exercicio 1 operação aritmética básica

---
+++ fundamentos/Bloco-04-JavaScript/dia-1-javascript-primeiros-passos/exercicio-01.js
@@ -0,0 +1,13 @@
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+//Adição
+//Subtração
+//Multiplicação
+//Divisão
+//Módulo
+let a = 8
+let b = 2
+console.log (a + b);
+console.log (a - b);
+console.log (a * b);
+console.log (a / b);
+console.log (a % b);
