933f78990dbc71fc560bdea19607e25f313a10e0
NataliaArmond <taya.armond@gmail.com>
2022-07-28 21:03:45 -0300

    resolvendo exercicios ate o 7

--- fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -12,15 +12,28 @@ const tagSection = document.createElement('Section');
 tagSection.className = 'center-content';
 tagMain.appendChild(tagSection);
 //4.🚀 Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
-
+let tagP = document.createElement('p');
+tagP.innerText = 'Criando um paragrafo ultilizando o Dom';
+tagSection.appendChild(tagP);
 //5.🚀 Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
-
+const tagSection1 = document.createElement('section');
+tagSection1.className = 'let-content';
+tagMain.appendChild(tagSection1);
 //6.🚀 Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
-
+const tagSection2 = document.createElement('section');
+tagSection2.className = 'right-content';
+tagMain.appendChild(tagSection2);
 //7.🚀 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
-
+const imagem = document.createElement('img');
+imagem.className = 'small-image';
+imagem.src = 'https://picsum.photos/200';
+tagSection1.appendChild(imagem);
 //8.🚀 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+const listaUl = document.createElement('ul')
 
+
+
+tagSection2.appendChild(listaUl);
 //9.🚀 Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
 
 //Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
